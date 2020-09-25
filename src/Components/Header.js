bc2b9eaf198db345708b642882a3865881139a11
sz16900 <sz16900@my.bristol.ac.uk>
2020-09-24 18:46:23 -1000

    header background red

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,7 +3,7 @@ import Navigation from './Navigation';
 
 function Header() {
   return (
-    <header className="border-b font- p-3 flex justify-between items-center">
+    <header className="border-b font- p-3 flex justify-between items-center bg-red-300">
       CloudNalu
       <span className="">
         <Navigation />
