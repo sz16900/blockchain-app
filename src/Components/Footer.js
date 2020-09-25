ff95f9a4cc7679e68f5cadfd6be71a657420d2aa
cloudnalu <68409311+cloudnalu@users.noreply.github.com>
2020-09-24 19:07:55 -1000

    footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer className="bg-gray-200 text-center text-xs p-3 absolute bottom-0 w-full">
+    <footer className="bg-red-200 text-center text-xs p-3 absolute bottom-0 w-full">
       &copy; Copyright 2020
     </footer>
   );
